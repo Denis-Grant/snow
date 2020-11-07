ba5252b67229fd3c50cad6dcb211d27c52651535
Apple <apple@MacbookPro.local>
2020-11-07 21:13:51 +0000

    complete

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  future: {
+    // removeDeprecatedGapUtilities: true,
+    // purgeLayersByDefault: true,
+  },
+  purge: [],
+  theme: {
+    extend: {
+      colors: {
+        lightgrey: "#F4F4F4",
+        mediumgrey: "#E6E6E6"
+      },
+      maxWidth: {
+        cardwidth: "730px"
+      }
+    },
+  },
+  variants: {},
+  plugins: [],
+}
